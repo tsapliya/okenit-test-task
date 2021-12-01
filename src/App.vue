a0b340fe3db1ca84c2753850a99654535a8c412a
tsapliya <s.ermishev@icloud.com>
2021-12-01 09:58:48 +0300

    небольшая стилизация 1я часть задания готова

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
+  <div class="messages">
     <span v-if="feed.length === 0">Сообщений нет</span>
     <message v-else v-for="(message, i) in feed" :key="i" :message="message"/>
+  </div>
 </template>
 
 <script>
@@ -25,5 +27,10 @@ export default {
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   color: #2c3e50;
+}
+
+.messages {
+  width: 70%;
+  margin: 0 auto;
 }
 </style>
